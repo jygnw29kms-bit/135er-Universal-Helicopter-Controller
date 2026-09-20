b32c61635e82d5fac8cdcc785bae8d5f5fe57ccc
jygnw29kms-bit <jygnw29kms@privaterelay.appleid.com>
2026-09-20 13:53:00 +0200

    Firmware V1.1: complete firmware/src/main.c

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,42 +3,74 @@
 #include "tusb.h"
 #include "axis.h"
 #include "calibration.h"
+#include "cli.h"
+#include "config.h"
 #include "hid_report.h"
+#include "inputs.h"
 #include "mcp3208.h"
+#include <string.h>
 
 static calibration_data_t cal;
+static axis_state_t axes[HELI_AXIS_COUNT];
 static heli_hid_report_t report;
+static heli_diagnostics_t diag;
 
-static void sample_axes(void){
-    uint16_t v[7];
-    for(int i=0;i<7;i++){
-        uint16_t raw=mcp3208_read_oversampled((uint8_t)i);
-        raw=axis_filter(&cal.axis[i],raw);
-        v[i]=axis_map(&cal.axis[i],raw);
+static void sample_axes(void) {
+    uint16_t value[HELI_AXIS_COUNT];
+    for (uint8_t i = 0; i < HELI_AXIS_COUNT; ++i) {
+        uint16_t raw = mcp3208_read_oversampled(i);
+        diag.latest_raw[i] = raw;
+        value[i] = axis_process(&axes[i], raw);
     }
-    report.cyclic_roll=v[0];
-    report.cyclic_pitch=v[1];
-    report.collective=v[2];
-    report.throttle=v[3];
-    report.pedals=v[4];
-    report.brake_left=v[5];
-    report.brake_right=v[6];
+
+    report.cyclic_roll = value[0];
+    report.cyclic_pitch = value[1];
+    report.collective = value[2];
+    report.throttle = value[3];
+    report.pedals = value[4];
+    report.brake_left = value[5];
+    report.brake_right = value[6];
 }
 
-int main(void){
+int main(void) {
     board_init();
     mcp3208_init();
+    inputs_init();
+
     calibration_load(&cal);
+    for (uint8_t i = 0; i < HELI_AXIS_COUNT; ++i)
+        axis_state_init(&axes[i], &cal.axis[i]);
+
     hid_report_init(&report);
+    memset(&diag, 0, sizeof(diag));
+    cli_init(&cal, axes, &diag);
+
     tusb_init();
 
-    absolute_time_t next=get_absolute_time();
-    while(1){
+    absolute_time_t next = delayed_by_us(get_absolute_time(), HELI_HID_INTERVAL_US);
+
+    while (true) {
         tud_task();
-        if(absolute_time_diff_us(get_absolute_time(),next)<=0){
-            next=delayed_by_us(next,1000);
+        cli_task();
+
+        if (time_reached(next)) {
+            int64_t lateness = -absolute_time_diff_us(get_absolute_time(), next);
+            if (lateness > (int64_t)HELI_HID_INTERVAL_US) {
+                ++diag.loop_overruns;
+                next = delayed_by_us(get_absolute_time(), HELI_HID_INTERVAL_US);
+            } else {
+                next = delayed_by_us(next, HELI_HID_INTERVAL_US);
+            }
+
+            inputs_task();
             sample_axes();
-            if(tud_hid_ready()) tud_hid_report(0,&report,sizeof(report));
+            report.buttons = inputs_buttons();
+            report.hat = inputs_hat();
+
+            if (tud_hid_ready()) {
+                tud_hid_report(0, &report, sizeof(report));
+                ++diag.report_count;
+            }
         }
     }
 }
