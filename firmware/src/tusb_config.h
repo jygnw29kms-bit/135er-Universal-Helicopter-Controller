bacbee0f498b3cda8d60d6fc037cfb98da2dcfb4
jygnw29kms-bit <jygnw29kms@privaterelay.appleid.com>
2026-09-20 13:53:03 +0200

    Firmware V1.1: complete firmware/src/tusb_config.h

--- firmware/src/tusb_config.h
+++ firmware/src/tusb_config.h
@@ -1,7 +1,18 @@
 #pragma once
+
+// Pico SDK's RP2 TinyUSB integration currently uses the RP2040 TinyUSB
+// controller backend for the RP2 USBCTRL peripheral family.
+#ifndef CFG_TUSB_MCU
 #define CFG_TUSB_MCU OPT_MCU_RP2040
+#endif
+
 #define CFG_TUSB_OS OPT_OS_NONE
 #define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
 #define CFG_TUD_ENDPOINT0_SIZE 64
+
 #define CFG_TUD_HID 1
 #define CFG_TUD_HID_EP_BUFSIZE 64
+
+#define CFG_TUD_CDC 1
+#define CFG_TUD_CDC_RX_BUFSIZE 128
+#define CFG_TUD_CDC_TX_BUFSIZE 128
