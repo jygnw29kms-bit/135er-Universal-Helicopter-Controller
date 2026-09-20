02b6b17375c1a19c9ed107846f282afeb3723c45
jygnw29kms-bit <jygnw29kms@privaterelay.appleid.com>
2026-09-20 12:41:00 +0200

    Update firmware/src/hid_report.h for V1 RC1

--- firmware/src/hid_report.h
+++ firmware/src/hid_report.h
@@ -1,16 +1,4 @@
 #pragma once
 #include <stdint.h>
-
-typedef struct __attribute__((packed)) {
-    uint16_t cyclic_roll;
-    uint16_t cyclic_pitch;
-    uint16_t collective;
-    uint16_t throttle;
-    uint16_t pedals;
-    uint16_t brake_left;
-    uint16_t brake_right;
-    uint32_t buttons;
-    uint8_t hat;
-} heli_hid_report_t;
-
-void hid_report_init(heli_hid_report_t *report);
+typedef struct __attribute__((packed)){uint16_t cyclic_roll,cyclic_pitch,collective,throttle,pedals,brake_left,brake_right;uint32_t buttons;uint8_t hat;} heli_hid_report_t;
+void hid_report_init(heli_hid_report_t*r);
