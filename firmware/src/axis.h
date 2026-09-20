f8ac81a51af93a177fc1de1bedd8d86b972b7815
jygnw29kms-bit <jygnw29kms@privaterelay.appleid.com>
2026-09-20 12:40:50 +0200

    Update firmware/src/axis.h for V1 RC1

--- firmware/src/axis.h
+++ firmware/src/axis.h
@@ -1,13 +1,7 @@
 #pragma once
 #include <stdint.h>
-
-typedef struct {
-    uint16_t raw_min;
-    uint16_t raw_center;
-    uint16_t raw_max;
-    uint16_t deadzone;
-    uint16_t filtered;
-} axis_state_t;
-
-uint16_t axis_apply_calibration(axis_state_t *axis, uint16_t raw);
-uint16_t axis_filter(axis_state_t *axis, uint16_t sample);
+#include <stdbool.h>
+typedef enum { AXIS_BIPOLAR=0, AXIS_UNIPOLAR=1 } axis_mode_t;
+typedef struct { uint16_t raw_min,raw_center,raw_max,filtered_raw,deadzone; axis_mode_t mode; bool invert; } axis_state_t;
+uint16_t axis_filter(axis_state_t *a,uint16_t sample);
+uint16_t axis_map(axis_state_t *a,uint16_t raw);
