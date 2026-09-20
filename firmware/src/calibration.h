67a8e71cd1be8c1a7fc886209795f12c84c46e95
jygnw29kms-bit <jygnw29kms@privaterelay.appleid.com>
2026-09-20 13:52:25 +0200

    Firmware V1.1: update firmware/src/calibration.h

--- firmware/src/calibration.h
+++ firmware/src/calibration.h
@@ -2,8 +2,18 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "axis.h"
-#define HELI_AXIS_COUNT 7
-typedef struct{uint32_t magic,version;axis_state_t axis[HELI_AXIS_COUNT];uint32_t crc32;} calibration_data_t;
-void calibration_defaults(calibration_data_t*d);
-bool calibration_load(calibration_data_t*d);
-bool calibration_save(const calibration_data_t*d);
+#include "config.h"
+
+#define CALIBRATION_VERSION 2u
+
+typedef struct {
+    uint32_t magic;
+    uint32_t version;
+    axis_config_t axis[HELI_AXIS_COUNT];
+    uint32_t crc32;
+} calibration_data_t;
+
+void calibration_defaults(calibration_data_t *data);
+bool calibration_validate(const calibration_data_t *data);
+bool calibration_load(calibration_data_t *data);
+bool calibration_save(calibration_data_t *data);
